0307b4e6c3f362f21948a86256301f2b248817f2
krishspicy31 <krishspicy31@gmail.com>
2025-06-24 15:44:56 +0530

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,11 +1,22 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import type { OpenNextConfig } from "@opennextjs/aws/types/open-next";
 import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
 
-export default defineCloudflareConfig({
-  incrementalCache: kvIncrementalCache,
-  tagCache: d1NextTagCache,
-  queue: doQueue,
-  enableCacheInterception: true
-});
+const config: OpenNextConfig = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: kvIncrementalCache,
+      tagCache: d1NextTagCache,
+      queue: doQueue,
+    }
+  },
+  dangerous: {
+    enableCacheInterception: true
+  }
+};
+
+export default defineCloudflareConfig(config);
